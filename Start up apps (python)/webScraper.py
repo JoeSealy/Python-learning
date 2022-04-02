faa231fc167d64c550e6fc753502536946b9c64c
@Joe_Sealy <joe.sealy@hotmail.co.uk>
2022-04-02 12:04:32 +0100

    SONE COUPLE THINGS
    forgot what they werew but changed quite a lot of things about

--- Start up apps (python)/webScraper.py
+++ Start up apps (python)/webScraper.py
@@ -1,29 +1,39 @@
+import imghdr
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 import smtplib
 import time
 import base64 
 import os
+
+from sympy import content
 import personal
+import shutil 
 from email.message import EmailMessage
+from bs4 import BeautifulSoup
 
 EMAIL_ADD = personal.EMAIL_ADDRESS
 EMAIL_PASS = personal.EMAIL_PASSWORD
+HEADER = personal.HEADER
 
 URL = "https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1311&_nkw=1070ti+graphics+card&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=GRAPHICS+CARDS&_osacat=0&LH_All=1"
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"}
 
 def data_get(URL):
-    request = requests.get(URL, headers = header)
+    request = requests.get(URL, headers = HEADER)
     soup = BeautifulSoup(request.text, "html.parser")
     return soup
 
 
 
 def data_find(soup):
-    message = []
-    results = soup.find_all("li", class_ = "s-item s-item__pl-on-bottom s-item--watch-at-corner", limit = 20)
+    imageList = []
+    productFeatureList = []
+    msg = EmailMessage()
+    msg['Subject'] = "Some Graphics cards Poo Boy"
+    msg['From'] = EMAIL_ADD
+    msg['To'] = "joe.sealy@hotmail.co.uk"
+
+    results = soup.find_all("li", class_ = "s-item s-item__pl-on-bottom s-item--watch-at-corner", limit = 2)
     for products in results:
 
         title = products.find("h3", class_ ="s-item__title").text
@@ -33,8 +43,18 @@ def data_find(soup):
         bids = products.find("span", class_ = "s-item__bids s-item__bidCount")
         buyItNow = products.find("span", class_ = "s-item__purchase-options-with-icon")
         link = products.find("a", class_ = "s-item__link")["href"]
+        imgLink = products.find("img", class_ = "s-item__image-img")["src"]
 
-        if buyItNow == None :
+        first_char = condition[0]
+        if(first_char == "O"):
+            condition = "Opened But Never Used"
+        elif(first_char == "P"):
+            condition = "Pre owned"  
+
+        sellingTemp = sellingPrice.split("£")
+        sellingPrice = (sellingTemp[1] + " Pounds")
+
+        if buyItNow == None:
             buyItNow = "NaN"
             bids = bids.text
             timeLeft = timeLeft.text
@@ -43,31 +63,43 @@ def data_find(soup):
             bids = "NaN"
             timeLeft = "NaN"
 
-        sellingTemp = sellingPrice.split("£")
-        AsciiSellingPoint = (sellingTemp[1] + " Pounds")
-
-        first_char = condition[0]
-        if(first_char == "O"):
-            condition = "Opened But Never Used"
-        elif(first_char == "P"):
-            condition = "Pre owned"
-
-        each_product = {
-            f"Title: {title}\n Condition: {condition}\n Selling Price: {AsciiSellingPoint}\n Time Left: {timeLeft}\n Bids: {bids}\n Buy it Now?: {buyItNow}\n Link: {link}\n"
-        }
-
-        message.append(each_product) 
+    
+        productFeatureList.append(f"Title: {title}\nCondition: {condition}\n"
+        f"Selling Price: {sellingPrice}\nTime Left: {timeLeft}\n"
+        f"Bids: {bids}\nBuy it Now?: {buyItNow}\nLink: {link}\n")
         
-    return message
+        image = imgEmbed(imgLink)
+        imageList.append(image)
 
-def send_mail(message):
-    msg = EmailMessage()
-    msg['Subject'] = "Some Graphics cards Poo Boy"
-    msg['From'] = EMAIL_ADD
-    msg['To'] = "joe.sealy@hotmail.co.uk"
-    msg.set_content(message)
+    msg.set_content(productFeatureList)
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    for image in imageList:
+        with open(image, "rb") as f:
+            file_data = f.read()
+            file_type = imghdr.what(f.name)
+            file_name = f.name
+        msg.add_attachment(file_data, maintype = "image", subtype = file_type, filename = file_name)
+
+    return msg
+
+def imgEmbed(img):
+    filename = img.split("/")[6] + ".jpg"
+    r = requests.get(img, stream = True)
+    
+    if r.status_code == 200:
+        r.raw.decode_content = True
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        
+        print('Image sucessfully Downloaded: ',filename)
+    else:
+        print('Image Couldn\'t be retreived')
+
+    return filename 
+
+def send_mail(msg):
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(EMAIL_ADD, EMAIL_PASS)
         smtp.send_message(msg)
         print("mail has been sent")
@@ -79,6 +111,5 @@ def send_mail(message):
 #   time.sleep(60)
 
 data = data_get(URL)
-message = data_find(data)
-print(message)
-#send_mail(message)
+msg = data_find(data)
+send_mail(msg)
